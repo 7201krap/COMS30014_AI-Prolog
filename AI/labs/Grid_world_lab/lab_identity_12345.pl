9e618000e14bd6bc5254cec2788cb75d33c34b02
7201krap <47752972+7201krap@users.noreply.github.com>
2021-01-07 16:41:56 +0000

    commented on lab_identity

--- AI/labs/Grid_world_lab/lab_identity_12345.pl
+++ AI/labs/Grid_world_lab/lab_identity_12345.pl
@@ -10,7 +10,7 @@
 % wt_link(+WikiText, -Link)	return any Link contained in the given WikiText
 % --> wt_link('[[Category:United States Coast Guard reservists]]', L).
 
-% agent_ask_oracle(+A, +Orac,+Qu,-Ans)	This is used to question the oracle. If the Agent's
+% agent_ask_oracle(+A, +Orac, +Qu, -Ans)	This is used to question the oracle. If the Agent's
 % Question is link then the Oracle's Answer will be a random link from the Wikipedia page
 % whose title is the (name of the) secret random actor identity
 
@@ -20,22 +20,45 @@
 
 % ------------------------------------------------------------------------------
 % find_identity(-A)
-% hi
+
+% actor_has_link(?L, ?A)
+% Here, we are going to use this functor as: actor_has_link(+L, -A)
 actor_has_link(L,A) :-
   actor(A),
   wp(A,WT),
-  wt_link(WT,L).
+  wt_link(WT,L).  % L contains any link contained in the given WikiText (WT)
 
-eliminate([A],A) :- !.
-eliminate(As,A)  :-
+% eliminate(+As, -A)
+eliminate([A], A) :-
+  write('\nFinal anwer: '),
+  write(A),
+  !. % base case
+
+eliminate(As, A)  :-
+  write("\nEntering eliminate\n"),
+
   % agent_ask_oracle(oscar,o(1),link,L) to gain a link L that belongs to the secret actor.
   % This link can be used to eliminate certain actors until eventually there is only one possible actor left.
+  % This means that we need to use agent_ask_oracle many times.
   agent_ask_oracle(oscar,o(1),link,L),
-  include(actor_has_link(L),As,ViableAs),
-  eliminate(ViableAs,A).
+
+  % include(:Goal, +List1, ?List2)
+  include(actor_has_link(L), As, ViableAs), % viable : 실행가능한
+
+  write("\nPossible Answer(Actor) after elimination: "),
+  write(As),
+  write("\n"),
+
+  eliminate(ViableAs, A).  % 여기에서 Answer(Actor) 를 찾는다. base case 를 이용하여 찾을 수 있다.
+
 
 find_identity(A) :-
   findall(A,actor(A),As),
+
+  write("Actor List: "),
+  write(As),
+  write("\n"),
+
   eliminate(As,A).
 
 % ------------------------------------------------------------------------------

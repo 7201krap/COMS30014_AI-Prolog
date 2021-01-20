5bda1bb820d267e9255aa6e2100b67089176b785
Jason Jinhyun Park <jp17528@bristol.ac.uk>
2021-01-20 11:22:30 +0000

    recap list

---
+++ AI/examples/list_recap.pl
@@ -0,0 +1,13 @@
+%% 예제 1 - print all elements in the list
+
+%% 예제 2 - find a specific element in the list
+
+%% 예제 3 - find a specific element in the list
+
+%% 예제 4 - print the first element of the list
+
+%% 예제 5 - 짝수만 골라내는 프로그램
+
+%% 예제 6 - 짝수만 골라내는 프로그램
+
+%% 예제 7 - find maximum value of a list
